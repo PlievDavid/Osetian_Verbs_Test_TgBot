ccd5fb8e6e923e1b580e97824a0eecef51fd68e1
AlanTsarakov <AlanTsarakov>
2025-12-09 13:16:25 +0300

    Тестирование Reply Keyboard Markup

--- OssetianVerbsTelegramBot/StartCommand.cs
+++ OssetianVerbsTelegramBot/StartCommand.cs
@@ -5,6 +5,13 @@ static class StartCommand
 {
     internal static async Task ExecuteAsync(ITelegramBotClient client, Update update)
     {
-        
+        if (update != null)
+        {
+            await client.SendMessage(update.Message.Chat.Id, "ммм", replyMarkup: new string[][]
+            {
+                    new string[]{"привет", "gdf"},
+                    new string[]{"ммм" } 
+            });
+        }
     }
 }
